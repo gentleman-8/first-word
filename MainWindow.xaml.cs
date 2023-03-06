aabfb1f8531cbe9c8217ee2847665f1b734f2f06
gentleman-8 <roc.no2004@gmail.com>
2023-03-06 14:38:44 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace first_word
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Initializ456465456456456456456456455456465465465465
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                89489489489/7fsadfjdjfasjdlfkldsjfka
+                dsfa4fds65fsd4564as65f456ad4f6546f
+                456ds456f456sd456d4s6546f5s
+                
+                
+                
+                sdaf6ds5f456sd4f546sda4f5ds
+                
+                
+                
+                fsad56sd4f564s56f65ds4f65f
+                
+                
+                
+                asdfadsfadsdsffdsfffuckyou             
+
+
+
+                46546546sd
+                f
+                sdf
+                0564645566456fd4654f56d4sf
+                d
+
+                df
+                asdf
+                da
+                f
+                dasf
+                ds
+                f
+                dsfa
+                df
+                fs4f56dsa454ds654
+        }
+    }
+}
